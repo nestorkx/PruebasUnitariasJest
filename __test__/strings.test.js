846a0112c00c51cec23697af30e0f07ea4d2b9bc
Nestor Kauil <nestor@cloudware.tech>
2018-11-29 17:45:19 -0500

    - Pruebas unitarias para las cadenas de texto

---
+++ __test__/strings.test.js
@@ -0,0 +1,14 @@
+describe('Comprobar cadenas de texto', () => {
+    const text = 'es bonito texto'
+    test('Debe contener el siguiente texto', () => {
+        expect(text).toMatch(/bonito/)
+    })
+    // Este va a fallar intencionalmente, porque tiene la palabra "es"
+    // y aqui le esta diciendo que no tiene
+    test('No contiene el siguiente texto', () => {
+        expect(text).not.toMatch(/es/)
+    })
+    test('Comprobar el tamaño de un texto', () => {
+        expect(text).toHaveLength(15)
+    })
+})
